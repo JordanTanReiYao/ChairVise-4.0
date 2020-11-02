2c828524071f5b4aa91d74b94a289755bd9d54b7
JJiahui <jiangjiahui2048@gmail.com>
2020-11-02 18:05:18 +0800

    softconf submission record, able to generate predefined mapping, able to upload

--- src/web/app/src/store/data/columnHeaders.js
+++ src/web/app/src/store/data/columnHeaders.js
@@ -3,5 +3,10 @@ export default {
         submission: [
             "#", "track #", "track name", "title", "authors", "submitted","last updated", "keywords", "decision", "notified", "reviews sent", "abstract"
         ]
+    },
+    softconf: {
+        submission: [
+            "Submission ID", null, "Track", "Title", "Authors", "Submission Date", null, null, "Acceptance Status", null, null, "Summary"
+        ]
     }
 }
